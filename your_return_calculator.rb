377d9331cdb9aab080cf058afd7550c120b0bd0a
George Utsin <georgeutsin@gmail.com>
2016-09-17 15:29:21 -0400

    Working calculator version
    Time permitting, will update to accommodate for edge cases

--- your_return_calculator.rb
+++ your_return_calculator.rb
@@ -2,14 +2,22 @@ require_relative 'return_calculator'
 
 class YourReturnCalculator < ReturnCalculator
   def calculate!
-    # Write your code here.
-    # You have access to the `snapshots` variable.
-    #
-    # You can access the following properties of a snapshot:
-    # snapshot.date
-    # snapshot.cash_flow
-    # snapshot.market_value
+    naive_returns = []
+    snapshots.each_with_index do |snapshot, index|
+      unless index == 0
+        naive_return = (snapshot.market_value - snapshot.cash_flow)/snapshots[index-1].market_value - 1
+        naive_returns.push(naive_return)
+      end
+    end
 
-    BigDecimal.new(0)
+    BigDecimal.new(geometric_link(naive_returns))
+  end
+
+  def geometric_link(naive_returns)
+    cur_return = 1
+    naive_returns.each do |naive_return|
+      cur_return *= 1 + naive_return
+    end
+    cur_return - 1
   end
 end
