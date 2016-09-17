19c86b0825bc7e20fd6f717ba872fc180807282d
Jon Mio <jon.mio@hotmail.com>
2016-09-17 15:31:40 -0400

    done

--- your_return_calculator.rb
+++ your_return_calculator.rb
@@ -2,14 +2,28 @@ require_relative 'return_calculator'
 
 class YourReturnCalculator < ReturnCalculator
   def calculate!
-    # Write your code here.
-    # You have access to the `snapshots` variable.
-    #
-    # You can access the following properties of a snapshot:
-    # snapshot.date
-    # snapshot.cash_flow
-    # snapshot.market_value
+    iterations = snapshots.length - 1
+    product = 1
+    if (iterations > 1)
+      prevMarket = snapshots.first.market_value
+    else
+      puts "Cannot calculate for period less than 2 days"
+      return 0
+    end
 
+    iterations.times do |subperiod|
+      currMarket = snapshots[subperiod+1].market_value
+      currCash = snapshots[subperiod+1].cash_flow
+      if prevMarket == 0
+        puts "Cannot divide by 0"
+        return product-1
+      end
+      #prevent rounding
+      product *= (currMarket-currCash)/prevMarket.to_f
+      prevMarket = currMarket
+    end
+    return product-1
+    ##???? BIG DECIMAL???
     BigDecimal.new(0)
   end
 end
