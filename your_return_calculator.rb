8037e309b298970c014d953a4226b03b6284005a
Andrew Yang <andrewyang96@gmail.com>
2016-09-17 15:24:51 -0400

    ez

--- your_return_calculator.rb
+++ your_return_calculator.rb
@@ -9,7 +9,15 @@ class YourReturnCalculator < ReturnCalculator
     # snapshot.date
     # snapshot.cash_flow
     # snapshot.market_value
-
-    BigDecimal.new(0)
+    previous_snapshot = nil
+    total_return = 1
+    for snapshot in snapshots do
+      if previous_snapshot != nil then
+        period_return = (snapshot.market_value - snapshot.cash_flow) / previous_snapshot.market_value
+        total_return = total_return * period_return
+      end
+      previous_snapshot = snapshot
+    end
+    return total_return - 1
   end
 end
