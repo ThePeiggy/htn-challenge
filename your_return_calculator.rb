3b21cad389928550a090bef8e5ac52e7e5f3c83d
Eric Bai <eric.bai@sigopt.com>
2016-09-17 15:21:51 -0400

    calculate twr

--- your_return_calculator.rb
+++ your_return_calculator.rb
@@ -9,7 +9,17 @@ class YourReturnCalculator < ReturnCalculator
     # snapshot.date
     # snapshot.cash_flow
     # snapshot.market_value
+    previous = snapshots[0].market_value
+    snapshots.shift
+    twr = 1
 
-    BigDecimal.new(0)
+    for s in snapshots
+      naive = (s.market_value - s.cash_flow) / previous
+      twr = twr * naive
+      previous = s.market_value
+    end
+
+    twr = twr - 1
+    return twr
   end
 end
